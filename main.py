bea8fa99673ab4eec7f0e7e410f701d59179a672
Sachin <sachnboora@gmail.com>
2022-06-02 12:34:48 +0530

    http get request completed

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
+import random
+
 from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-##Connect to Database
+# Connect to Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-
-##Cafe TABLE Configuration
+# Cafe TABLE Configuration
 class Cafe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(250), unique=True, nullable=False)
@@ -28,6 +29,76 @@ class Cafe(db.Model):
 def home():
     return render_template("index.html")
     
+
+# This route is used to get a random cafe from the list
+@app.route("/random", methods=["GET"])
+def get_random_cafe():
+    cafes = db.session.query(Cafe).all()
+    random_cafe = random.choice(cafes)
+    return jsonify(
+        cafe={
+            "id": random_cafe.id,
+            "name": random_cafe.name,
+            "map_url": random_cafe.map_url,
+            "img_url": random_cafe.img_url,
+            "location": random_cafe.location,
+            "seats": random_cafe.seats,
+            "has_toilet": random_cafe.has_toilet,
+            "has_wifi": random_cafe.has_wifi,
+            "has_sockets": random_cafe.has_sockets,
+            "can_take_calls": random_cafe.can_take_calls,
+            "coffee_price": random_cafe.coffee_price
+        }
+    )
+
+
+# This route is used to get all the cafes in the list
+@app.route("/all", methods=["GET"])
+def all_cafes():
+    cafes = db.session.query(Cafe).all()
+    cafe_list = []
+    for cafe in cafes:
+        cafe_dict = {
+            "id": cafe.id,
+            "name": cafe.name,
+            "map_url": cafe.map_url,
+            "img_url": cafe.img_url,
+            "location": cafe.location,
+            "seats": cafe.seats,
+            "has_toilet": cafe.has_toilet,
+            "has_wifi": cafe.has_wifi,
+            "has_sockets": cafe.has_sockets,
+            "can_take_calls": cafe.can_take_calls,
+            "coffee_price": cafe.coffee_price
+        }
+        cafe_list.append(cafe_dict)
+    return jsonify(cafes=cafe_list)
+
+
+# This route is used to search all cafes in user searched location
+@app.route("/search", methods=["GET"])
+def search_cafe_at_location():
+    query_location = request.args.get("loc")
+    cafe = db.session.query(Cafe).filter_by(location = query_location).first()
+    cafe_dict = {
+        "id": cafe.id,
+        "name": cafe.name,
+        "map_url": cafe.map_url,
+        "img_url": cafe.img_url,
+        "location": cafe.location,
+        "seats": cafe.seats,
+        "has_toilet": cafe.has_toilet,
+        "has_wifi": cafe.has_wifi,
+        "has_sockets": cafe.has_sockets,
+        "can_take_calls": cafe.can_take_calls,
+        "coffee_price": cafe.coffee_price
+    }
+    if cafe:
+        return jsonify(cafe = cafe_dict)
+    else:
+        return jsonify(error={"Not Found": "Sorry, we don't have a cafe at that location."})
+
+
 
 ## HTTP GET - Read Record
 
